4ecd9844c94aeddd9f69117c42585160a29c62ea
4archon <raushan.galiev9@yandex.ru>
2024-05-11 22:20:55 +0300

    first

---
+++ get_ast.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+	"errors"
+)
+
+func tokenDir(scandir string, fset *token.FileSet) error {
+	dir, err := os.ReadDir(scandir)
+	if err != nil {
+		fmt.Println("Read dir error")
+		return errors.New("Read dir error")
+	}
+	for _, file := range dir {
+		var fileName string = scandir + file.Name()
+		stat, err := os.Stat(fileName)
+		if err != nil {
+			fmt.Println("Read stst error")
+			return errors.New("Read stst error")
+		}
+		(*fset).AddFile(fileName, -1, int(stat.Size()))
+	}
+	return nil
+}
+
+func analize(node ast.Node) bool {
+	var name string;
+	switch x := node.(type) {
+	case *ast.FuncDecl:
+		name = x.Name.Name
+	}
+	if name != "" {
+		fmt.Println(name)
+	}
+	return true
+}
+
+func main() {
+	var astPrint string;
+	if len(os.Args) == 2 {
+		astPrint = os.Args[1]
+	}
+
+	var scandir string = "tests/"
+	fset := token.NewFileSet()
+
+
+	node, err := parser.ParseDir(fset, scandir, nil, 0)
+	if err != nil {
+		fmt.Println("Parsing error")
+		os.Exit(1)
+	}
+
+	if astPrint == "ast" {
+		ast.Fprint(os.Stdout, fset, node, nil)
+	} else if astPrint == "anl" {
+		ast.Inspect(node["main"], analize)
+	}
+
+}
---
+++ tests/test.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func foo() int {
+	var i2 int = 100
+	return i2
+}
+
+func main() {
+	var i1 int = 20
+	fmt.Println(i1)
+}
