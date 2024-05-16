60fd1e16f16dc7e51594f3b072b8c2632e68c4af
4archon <raushan.galiev9@yandex.ru>
2024-05-17 02:04:31 +0300

    storage added

--- get_ast.go
+++ get_ast.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"go/ast"
 	"go/parser"
 	"go/token"
 	"os"
-	"errors"
 )
 
 func tokenDir(scandir string, fset *token.FileSet) error {
@@ -27,32 +27,84 @@ func tokenDir(scandir string, fset *token.FileSet) error {
 	return nil
 }
 
-type list []string
+type varProg struct {
+	name string
+	typeVar string
+	value *int
+	pos token.Pos
+}
 
-func (l *list) analize(node ast.Node) bool {
-	var name string;
+// func getLine(fset token.FileSet, pos token.Pos) int {
+	
+// }
+
+type storage map[string]map[string][]varProg
+
+func getType(node *ast.StarExpr) (string) {
+	switch x := node.X.(type) {
+	case *ast.Ident:
+		return "*" + x.Name
+	}
+	return ""
+}
+
+func (store *storage) analizeDeclStmt(node *ast.GenDecl, funcName string) {
+	for _, i := range node.Specs {
+		switch x := i.(type) {
+		case *ast.ValueSpec:
+			switch y := x.Type.(type) {
+			case *ast.StarExpr:
+				var s varProg
+				s.name = x.Names[0].Name
+				s.typeVar = getType(y)
+				s.pos = x.Names[0].NamePos
+				s.value = nil
+				(*store)[funcName][x.Names[0].Name] = append((*store)[funcName][x.Names[0].Name], s)
+			}
+		}
+	}
+}
+
+func (store *storage) analizeFuncBody(node *ast.BlockStmt, funcName string) {
+	(*store)[funcName] = make(map[string][]varProg)
+	for _, i := range node.List {
+		switch x := i.(type) {
+		case *ast.DeclStmt:
+			switch y := x.Decl.(type) {
+			case *ast.GenDecl:
+				store.analizeDeclStmt(y, funcName)
+			}
+		}
+	}
+}
+
+func (store *storage) storeVar(node ast.Node) bool {
 	switch x := node.(type) {
 	case *ast.FuncDecl:
-		name = x.Name.Name
-	}
-	if name != "" {
-		*l = append(*l, name)
+		(*store).analizeFuncBody(x.Body, x.Name.Name)
 	}
 	return true
 }
 
-type varProg struct {
-	name string
-	typeVar string
-	value string
-	pos string
+func getLine(fset *token.FileSet, pos token.Pos) int {
+	return fset.Position(pos).Line
 }
 
-type storage map[string]map[string][]varProg
-
-func (store *storage) storeVar(node *ast.Node) bool {
-	
+func (store storage) printStore(fset *token.FileSet) {
+	for _, i := range store {
+		for _, j := range i {
+			for _, k := range j {
+				fmt.Println(k.name)
+				fmt.Println(getLine(fset, k.pos))
+				fmt.Println(k.typeVar)
+				fmt.Println(k.value)
+				fmt.Println("--------")
+			}
+		}
+	}
 }
+
+// func ge
 
 func main() {
 	var astPrint string;
@@ -73,9 +125,10 @@ func main() {
 	if astPrint == "ast" {
 		ast.Fprint(os.Stdout, fset, node, nil)
 	} else if astPrint == "anl" {
-		var l list
-		ast.Inspect(node["main"], l.analize)
-		fmt.Println(l)
+		s := make(storage)
+		ast.Inspect(node["main"], s.storeVar)
+		// fmt.Println(s["main"]["i2"][0].)
+		s.printStore(fset)
 	}
 
 }
--- tests/test.go
+++ tests/test.go
@@ -2,21 +2,17 @@ package main
 
 import "fmt"
 
-func foo() int {
-	var i2 int = 100
-	return i2
+func foo() *int {
+	return nil
 }
 
 func main() {
 	var i1 int = 20
 	i1 = 30
-	i1++
-	i1--
-	i1 = i1 + 100
-	i1 += 22
-	i1 = foo()
 	var i2 *int
 	i2 = &i1
+	i3 := foo()
 	fmt.Println(i1)
 	fmt.Println(*i2)
+	fmt.Println(*i3)
 }
